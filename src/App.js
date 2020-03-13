3cc6a37b359fcc8b9caef0406da1c8d398b1cc2f
Vincent Costa <costa.vincent132@gmail.com>
2020-03-12 20:48:14 -0700

    boilerplate

--- src/App.js
+++ src/App.js
@@ -8,16 +8,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          SecureChat
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+          Encrypted channels with unlimited users
+        </p>
       </header>
     </div>
   );
