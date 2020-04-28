46024d68a954f46fc83eb792c2fcc9503e1d8a12
Vincent Costa <costa.vincent132@gmail.com>
2020-04-27 20:08:56 -0700

    Add local video to client

--- src/actions/callActions.js
+++ src/actions/callActions.js
@@ -82,6 +82,9 @@ export const acceptCall = (data, offer) => dispatch => {
 
     navigator.mediaDevices.getUserMedia(constraints)
             .then(stream => {
+                const audioElement = document.querySelector("video#localVideo")
+                audioElement.srcObject = stream
+
                 dispatch({
                     type: ACCEPT_CALL,
                     data,
